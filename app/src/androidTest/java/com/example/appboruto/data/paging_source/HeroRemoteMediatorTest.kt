f7ec00b3a208bb9359be8b0ddcecf09cc6203647
Ana Gomes Souza <agomesou@emeal.nttdata.com>
2022-07-18 16:55:42 -0300

    Aplicando testes UI

--- app/src/androidTest/java/com/example/appboruto/data/paging_source/HeroRemoteMediatorTest.kt
+++ app/src/androidTest/java/com/example/appboruto/data/paging_source/HeroRemoteMediatorTest.kt
@@ -1,4 +1,91 @@
 package com.example.appboruto.data.paging_source
 
+import androidx.paging.*
+import androidx.test.core.app.ApplicationProvider
+import com.example.appboruto.data.local.BorutoDatabase
+import com.example.appboruto.data.remote.FakeBorutoApi2
+import com.example.appboruto.domain.model.Hero
+import junit.framework.TestCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
 class HeroRemoteMediatorTest {
+    private lateinit var borutoApi: FakeBorutoApi2
+    private lateinit var borutoDatabase: BorutoDatabase
+
+    @Before
+    fun setup() {
+        borutoApi = FakeBorutoApi2()
+        borutoDatabase = BorutoDatabase.create(
+            context = ApplicationProvider.getApplicationContext(),
+            useInMemory = true
+        )
+    }
+
+    @After
+    fun cleanup() {
+        borutoDatabase.clearAllTables()
+    }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadReturnsSuccessResultWhenMoreDataIsPresent() =
+        runBlocking {
+            val remoteMediator = HeroRemoteMediator(
+                borutoApi = borutoApi,
+                borutoDatabase = borutoDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            TestCase.assertTrue(result is RemoteMediator.MediatorResult.Success)
+            TestCase.assertFalse((result as RemoteMediator.MediatorResult.Success).endOfPaginationReached)
+        }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadSuccessAndEndOfPaginationTrueWhenNoMoreData() =
+        runBlocking {
+            borutoApi.clearData()
+            val remoteMediator = HeroRemoteMediator(
+                borutoApi = borutoApi,
+                borutoDatabase = borutoDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            TestCase.assertTrue(result is RemoteMediator.MediatorResult.Success)
+            TestCase.assertTrue((result as RemoteMediator.MediatorResult.Success).endOfPaginationReached)
+        }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadReturnsErrorResultWhenErrorOccurs() =
+        runBlocking {
+            borutoApi.addException()
+            val remoteMediator = HeroRemoteMediator(
+                borutoApi = borutoApi,
+                borutoDatabase = borutoDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            TestCase.assertTrue(result is RemoteMediator.MediatorResult.Error)
+        }
 }
